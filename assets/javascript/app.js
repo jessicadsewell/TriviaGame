93f734d66b1927d4daa8baa853161a2b0b6fde71
Jessica Sewell <jessicadawnsewell@gmail.com>
2019-06-05 21:05:17 -0600

    Addition of trivia questions and title

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+
+// Pseudo code 
+
+// Trivia answers: http://murphysamandjodi.com/the-ultimate-star-wars-fan-trivia-answers/
+// * You'll create a trivia form with multiple choice or true/false options (your choice).
+
+// * The player will have a limited amount of time to finish the quiz. 
+
+//  * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+// * Don't let the player pick more than one answer per question.
+
+// * Don't forget to include a countdown timer.
+
+// Variables 
+// countdown timer beginning at 120 sec, counts down immediately 
+// correctnum
+// incorrectnum
+// unanswerednum
+// start button
+// done button 
+// user guess
+// question 1, 2, 3, 4, 5, 6
+
+// if user guess clicks on correct answer (index number) 
+// then correctnum ++
+
+// if userguess clicks on incorrect answer 
+// then incorrectnum ++
+
+// if question is not answered
+// then unanswernum ++ 
+
+// if countdown = 0
+// display correctnum, incorrect num, and unanswerednum
+
+// if done button is clicked, display correctnum, incorrect num and unanswerednum
+
+$(document).ready(function () {
+
+});
